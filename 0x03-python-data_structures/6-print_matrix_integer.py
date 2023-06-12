524a95b7714bd9a9efc5d1c7b12ab144835af839
mmolalekan <muhayminolalekan@yahoo.com>
2023-06-12 20:09:47 +0100

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,11 @@
 def print_matrix_integer(matrix=[[]]):
     counter_i = len(matrix)
     counter_j = len(matrix[0])
-
-    for i in range(counter_i):
-        for j in range(counter_j - 1):
-            print("{} ".format(matrix[i][j]), end="")
-        if counter_j > 0:
-            print("{}".format(matrix[i][counter_j - 1]))
-        else:
-            print()
+    if isinstance(matrix, list):
+        for i in range(counter_i):
+            for j in range(counter_j - 1):
+                print("{:d} ".format(matrix[i][j]), end="")
+            if counter_j > 0:
+                print("{:d}".format(matrix[i][counter_j - 1]))
+            else:
+                print()

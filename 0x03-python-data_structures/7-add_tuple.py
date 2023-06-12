995316664f394384ebd6208823ec183bcd71693a
mmolalekan <muhayminolalekan@yahoo.com>
2023-06-12 20:56:57 +0100

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if isinstance(tuple_a, tuple) and isinstance(tuple_b, tuple):
-        a = [0, 0]
-        b = [0, 0]
-        for index, i in enumerate(tuple_a):
+    a = [0, 0]
+    b = [0, 0]
+    for index, i in enumerate(tuple_a):
+        if (index < 2):
             a[index] = tuple_a[index]
 
-        for index, i in enumerate(tuple_b):
+    for index, i in enumerate(tuple_b):
+        if (index < 2):
             b[index] = tuple_b[index]
-        sum_a = a[0] + b[0]
-        sum_b = a[1] + b[1]
-        return (sum_a, sum_b)
+    sum_a = a[0] + b[0]
+    sum_b = a[1] + b[1]
+    return (sum_a, sum_b)
--- 0x03-python-data_structures/7-main.py
+++ 0x03-python-data_structures/7-main.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 add_tuple = __import__('7-add_tuple').add_tuple
 
-tuple_a = (1, 89)
-tuple_b = (88, 11)
+tuple_a = (1, 2)
+tuple_b = (1, 2, 3)
 new_tuple = add_tuple(tuple_a, tuple_b)
 print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))

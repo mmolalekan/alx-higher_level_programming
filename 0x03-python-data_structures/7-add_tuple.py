bc351df9be49e2a00eb1156e3ac9b9d3d75771cb
mmolalekan <muhayminolalekan@yahoo.com>
2023-06-12 20:13:51 +0100

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = [0, 0]
-    b = [0, 0]
-    for index, i in enumerate(tuple_a):
-        a[index] = tuple_a[index]
+    if isinstance(tuple_a, tuple) and isinstance(tuple_b, tuple):
+        a = [0, 0]
+        b = [0, 0]
+        for index, i in enumerate(tuple_a):
+            a[index] = tuple_a[index]
 
-    for index, i in enumerate(tuple_b):
-        b[index] = tuple_b[index]
-    sum_a = a[0] + b[0]
-    sum_b = a[1] + b[1]
-    return (sum_a, sum_b)
+        for index, i in enumerate(tuple_b):
+            b[index] = tuple_b[index]
+        sum_a = a[0] + b[0]
+        sum_b = a[1] + b[1]
+        return (sum_a, sum_b)

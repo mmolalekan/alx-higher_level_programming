e6eea6b34672dc1b0da9197483ae13d672a14c16
mmolalekan <muhayminolalekan@yahoo.com>
2023-06-15 04:39:02 +0100

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,31 +1,14 @@
-#!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_list = list(roman_string)
-    total = curr = prev = 0
-    sum_list = []
-    for c in roman_list:
-        if c == 'I':
-            sum_list.append(1)
-        elif c == 'V':
-            sum_list.append(5)
-        elif c == 'X':
-            sum_list.append(10)
-        elif c == 'L':
-            sum_list.append(50)
-        elif c == 'C':
-            sum_list.append(100)
-        elif c == 'D':
-            sum_list.append(500)
-        elif c == 'M':
-            sum_list.append(1000)
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    prev_value = 0
 
-    for i in sum_list:
-        curr = i
-        if len(sum_list) == 1:
-            return i
-        elif curr > prev: 
-            total += curr - (2 * prev)
+    for c in roman_string:
+        current_value = roman.get(c, 0)
+        if current_value > prev_value:
+            total += current_value - 2 * prev_value
         else:
-            total += curr
-        prev = curr
+            total += current_value
+        prev_value = current_value
+
     return total

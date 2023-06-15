c82ec694da43a3f16980ed2840f9e2cd6c0327b4
mmolalekan <muhayminolalekan@yahoo.com>
2023-06-15 04:28:41 +0100

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if a_dictionary is None:
-        return (None)
-    else:
-        return max(a_dictionary)
+def best_score(my_dict):
+    if my_dict:
+        return (max(my_dict))

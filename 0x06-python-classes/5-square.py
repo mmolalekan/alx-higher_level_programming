a81e34094299363382ea7178976f5ea1daa21c11
mmolalekan <muhayminolalekan@yahoo.com>
2023-06-28 04:18:41 +0100

    project 0x06

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if isinstance(size, int) and size >= 0:
+            self.__size = size
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return (self.__size**2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print("#", end='')
+                print()
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    def area(self):
+        return (self.__size**2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for i in range(self.__position[0]):
+                        if (self.__position[1] > 0):
+                            print("_", end='')
+                        else:
+                            print(" ", end='')
+                for i in range(self.__size):
+                    print("#", end='')
+                print()
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value

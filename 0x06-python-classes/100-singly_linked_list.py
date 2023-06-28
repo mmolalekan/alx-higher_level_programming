1f82d0d5223f5cec91685885456e5572e248cdf0
mmolalekan <muhayminolalekan@yahoo.com>
2023-06-28 04:30:27 +0100

    project 0x06

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,36 +1,76 @@
+#!/usr/bin/python3
+"""Define classes for a singly-linked list."""
+
+
 class Node:
-    '''This is a class denoting a Node'''
+    """Represent a node in a singly-linked list."""
+
     def __init__(self, data, next_node=None):
-        '''This is the initialiser method'''
+        """Initialize a new Node.
+        Args:
+            data (int): The data of the new Node.
+            next_node (Node): The next node of the new Node.
+        """
         self.data = data
         self.next_node = next_node
 
     @property
-    ''' retrieving the value private value of data '''
     def data(self):
-        return self.__data
+        """Get/set the data of the Node."""
+        return (self.__data)
+
     @data.setter
     def data(self, value):
-        if isinstance(value, int):
-            self.__data = value
-        else:
+        if not isinstance(value, int):
             raise TypeError("data must be an integer")
+        self.__data = value
 
     @property
-    ''' retrieving the value private value of next_node '''
     def next_node(self):
-        return self.__next_node
+        """Get/set the next_node of the Node."""
+        return (self.__next_node)
+
     @next_node.setter
     def next_node(self, value):
-        if isinstance(value, int):
-            self.__next_node = value
-        elif value == None:
-            self.__next_node = None
-        else:
+        if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
 
 class SinglyLinkedList:
-    '''This is a class denoting a singly linked list'''
-    def __init__(self, head):
-        self.__head = head
-        
+    """Represent a singly-linked list."""
+
+    def __init__(self):
+        """Initalize a new SinglyLinkedList."""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """Insert a new Node to the SinglyLinkedList.
+        The node is inserted into the list at the correct
+        ordered numerical position.
+        Args:
+            value (Node): The new Node to insert.
+        """
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
+
+    def __str__(self):
+        """Define the print() representation of a SinglyLinkedList."""
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ('\n'.join(values))
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size=0):
-        if isinstance(size, int) and size >= 0:
-            self.__size = size
-        elif not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+"""Define a class Square."""
 
-    def area(self):
-        return (self.__size**2)
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
 
     @property
     def size(self):
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
@@ -21,5 +23,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)

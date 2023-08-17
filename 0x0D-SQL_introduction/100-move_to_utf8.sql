956ffb1e118db5501f1718f367c870b408e4bb41
mmolalekan <muhayminolalekan@yahoo.com>
2023-08-17 14:45:20 +0100

    task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -9,4 +9,3 @@ ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode
 
 -- Change character set for the field
 ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-

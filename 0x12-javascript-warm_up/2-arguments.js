5c5bc6c59e65f772bcf7dbf743d995f45f1834fb
mmolalekan <muhayminolalekan@yahoo.com>
2023-09-12 04:22:07 +0100

    task 0 and 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const argv = process.argv;
 if (argv.length === 2) {
   console.log('No argument');
 } else if (argv.length === 3) {
-  console.log('argument found');
-} else if (argv.length > 3) {
+  console.log('Argument found');
+} else {
   console.log('Arguments found');
 }

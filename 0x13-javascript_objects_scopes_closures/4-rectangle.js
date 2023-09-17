aec8757fd201763d4aa45f0c7bc160c92aaab274
muhaymin <muhayminolalekan@yahoo.com>
2023-09-17 03:57:36 +0100

    task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -6,6 +6,7 @@ class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       let myRow = '';
@@ -15,11 +16,13 @@ class Rectangle {
       console.log(myRow);
     }
   }
+
   rotate () {
-    let tmp = this.height;
+    const tmp = this.height;
     this.height = this.width;
     this.width = tmp;
   }
+
   double () {
     this.height = this.height * 2;
     this.width = this.width * 2;

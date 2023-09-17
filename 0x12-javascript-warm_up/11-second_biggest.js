541121ea8b287b1610118f29d31c57b304cbf9d3
muhaymin <muhayminolalekan@yahoo.com>
2023-09-17 01:48:26 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,19 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
+// if (process.argv.length <= 3) {
+//   console.log('0');
+// } else {
+//   const arr = process.argv.slice(2).map(Number);
+//   console.log(arr.sort((a, b) => a - b)[arr.length - 2]);
+// }
+const arr = process.argv;
+if (arr.length <= 3) {
   console.log('0');
 } else {
-  const arr = process.argv.slice(2).map(Number);
-  console.log(arr.sort((a, b) => a - b)[arr.length - 2]);
+  const arrNo = [];
+  for (let i = 2; i < arr.length; i++) {
+    arrNo.push(parseInt(arr[i]));
+  }
+  const max = Math.max(...arrNo);
+  const newArray = arrNo.filter(item => item !== max);
+  console.log(Math.max(...newArray));
 }

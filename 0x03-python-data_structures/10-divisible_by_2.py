d707f4088d3792921710c83fc7adb116b77ea15a
muhaymin <muhayminolalekan@yahoo.com>
2023-10-24 00:59:22 +0100

    new ways

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -7,3 +7,9 @@ def divisible_by_2(my_list=[]):
         else:
             new_list.append(False)
     return (new_list)
+
+"""
+def divisible_by_2(my_list=[]):
+    new_list = [True if x % 2 == 0 else False for x in my_list]
+    return new_list
+"""

757d3152b3db6d1f72bac8faa568b9f5ba15e869
mmolalekan <muhayminolalekan@yahoo.com>
2023-12-16 18:41:24 +0100

    correction

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, 
-#displays the size of the body of the response
-curl -sI "$1" | grep Content-Length | cut -d: -f2 | tr -d " "
+# takes in a URL, sends a request to that URL, displays the size of the body of the response
+curl -sI "$1" 2>/dev/null | grep Content-Length: | tr -d '[:space:]' | cut -d':' -f2

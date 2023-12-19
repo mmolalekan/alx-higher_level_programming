87817a9d1a037d3176cf597be24674d5c0c6288d
mmolalekan <muhayminolalekan@yahoo.com>
2023-12-19 10:16:43 +0100

    taks 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,10 +3,10 @@
 import requests
 import sys
 
-url = sys.argv[1]
-r = requests.get(url)
-if r.status_code >= 400:
-    print("Error code:", r.status_code)
-else:
-    print(r.text)
-
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code:", r.status_code)
+    else:
+        print(r.text)

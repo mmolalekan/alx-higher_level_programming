52db2a21c54734b8be6ff904d326e0ea94bb9d06
mmolalekan <muhayminolalekan@yahoo.com>
2023-12-19 10:07:35 +0100

    task 7'

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-import sys
+"""This module fetches https://alx-intranet.hbtn.io/status"""
 import requests
-"""
-This module takes in a URL, 
-sends a request to the URL and displays the body of the response.
-"""
+import sys
 
+url = sys.argv[1]
+r = requests.get(url)
+if r.status_code >= 400:
+    print("Error code:", r.status_code)
+else:
+    print(r.text)
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    response = requests.get(url)
-    if (response.status_code >= 400):
-        print("Error code: {}".format(response.status_code))
-    else:
-        print(response.text)
